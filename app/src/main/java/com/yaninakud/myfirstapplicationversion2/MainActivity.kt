511b6a97b5d2f537c9e3ef3934ec07678c70c39f
YaninaKud <ya_kudze@mail.ru>
2021-12-05 10:32:06 +0300

    new botton and text

---
+++ app/src/main/java/com/yaninakud/myfirstapplicationversion2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yaninakud.myfirstapplicationversion2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+    }
+}
